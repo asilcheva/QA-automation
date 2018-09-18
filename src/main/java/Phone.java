f33ba263746da0c343647ca3c4a329a6a92e06e4
asilcheva <30909521+asilcheva@users.noreply.github.com>
2018-09-18 17:41:22 +0300

    Add files via upload

---
+++ src/main/java/Phone.java
@@ -0,0 +1,21 @@
+public class Phone {
+    private int price;
+    private int capacity;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
