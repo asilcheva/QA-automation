b076cdd936ce4e68246131759eb82906d7d1a254
asilcheva <30909521+asilcheva@users.noreply.github.com>
2018-10-11 17:57:49 +0300

    Update SeleniumExample.java

--- src/test/java/SeleniumExample.java
+++ src/test/java/SeleniumExample.java
@@ -1,14 +1,42 @@
+
+
+
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.awt.event.WindowEvent;
 
 public class SeleniumExample {
-    @Test
-    public void initBrowser()
-    {
-        System.setProperty("webdriver.chrome.driver","C:\\server\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com.ua/");
-        driver.close();
+    private WebDriver driver;
+    @FindBy(xpath = "(//*[@class = 'ui-slider-handle ui-state-default ui-corner-all'])[1]")
+    private WebElement rigthPoint;
+    @FindBy(xpath = "//div[@class='layered_slider ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all']/a[2]")
+    private WebElement leftPoint;
+    @FindBy(xpath = "//*[@class = 'layered_slider ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all']")
+    private WebElement slider;
+    public SeleniumExample(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
+
+    public void calculation(int rigthPrice, int leftPrice){
+    int sliderWidth = slider.getSize().getWidth();
+    int widthPerOneDollar = (int)(sliderWidth/(53-16));
+    int rigthOffset = (rigthPrice-16)*widthPerOneDollar;
+    int leftOffset = (53-leftPrice)*widthPerOneDollar;
+    Actions action = new Actions(driver);
+    new WebDriverWait(driver, 10).until(ExpectedConditions. visibilityOf(rigthPoint));
+    new WebDriverWait(driver, 10).until(ExpectedConditions. visibilityOf(leftPoint));
+    action.dragAndDropBy(rigthPoint, rigthOffset, 0).build().perform();
+    action.dragAndDropBy(leftPoint, - leftOffset, 0).build().perform();
+    }
+
 }
