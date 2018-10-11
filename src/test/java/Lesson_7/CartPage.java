854d9b76859dfb3f5da5f98a59c35f57271b849f
asilcheva <30909521+asilcheva@users.noreply.github.com>
2018-10-11 11:22:02 +0300

    Update CartPage.java

--- src/test/java/Lesson_7/CartPage.java
+++ src/test/java/Lesson_7/CartPage.java
@@ -4,53 +4,91 @@ import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
+
+import java.math.BigDecimal;
 
 public class CartPage {
     @FindBy(id = "summary_products_quantity")
     private WebElement actualProductItemsQty;
-    @FindBy(id = "product_price_7_34_0")
+    @FindBy(xpath = "(//*[contains(@class, 'cart_item')]//*[contains(@class, 'cart_total')]//*[contains(@class, 'price')])[1]")
     private WebElement actualPriceOfFirstItem;
-    @FindBy(id = "product_price_3_13_0")
+    @FindBy(xpath = "(//*[contains(@class, 'cart_item')]//*[contains(@class, 'cart_total')]//*[contains(@class, 'price')])[2]")
     private WebElement actualPriceOfSecondItem;
-    @FindBy(id = "total_price")
-    private WebElement totalPrice;
+
+    public WebElement getFirstPriceWithDiscount() {
+        return firstPriceWithDiscount;
+    }
+
+    @FindBy(id = "price special-price")
+    private WebElement firstPriceWithDiscount;
+    @FindBy(id = "total_product")
+    private WebElement totalProducts;
     @FindBy(className = "old-price")
     private WebElement oldPrice;
-    @FindBy(className = "price-percent-reduction small")
+    @FindBy(xpath = "//*[@class='price-percent-reduction small']")
     private WebElement percent;
     @FindBy(className = "button btn btn-default standard-checkout button-medium")
     private WebElement proceedToCheckOut;
-
+    @FindBy(xpath = "//*[@class = 'address_firstname address_lastname']")
+    private WebElement actualUserNameOnBillingAddressStep;
+    @FindBy(className = "address_address1 address_address2")
+    private WebElement actualAddressOnBillingAddressStep;
+    @FindBy(className = "address_city address_state_name address_postcode")
+    private WebElement actualCityStatePostCodeOnBillingAddressStep;
+    @FindBy(className = "address_country_name")
+    private WebElement actualCountry;
+    @FindBy(className = "address_phone_mobile")
+    private WebElement actualMobilePhone;
     public CartPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
-
-    public String getProductItemsQty() {
-        return actualProductItemsQty.getText();
+    public WebElement getProceedToCheckOut() {
+        return proceedToCheckOut;
+    }
+    public String getActualUserNameOnBillingAddressStep() {
+        return actualUserNameOnBillingAddressStep.getText();
     }
 
-    public double getTotalPriceOfItems() {
-        double priceOfFirstItem = parsePrice(actualPriceOfFirstItem);
-        double priceOfSecondItem = parsePrice(actualPriceOfSecondItem);
-        return priceOfFirstItem + priceOfSecondItem;
+    public String getActualAddressOnBillingAddressStep() {
+        return actualAddressOnBillingAddressStep.getText();
     }
 
-    private double parsePrice(WebElement item) {
-        return Double.parseDouble(item.getText().substring(1));
+    public String getActualCityStatePostCodeOnBillingAddressStep() {
+        return actualCityStatePostCodeOnBillingAddressStep.getText();
     }
 
-    public double getTotsProductsPrice() {
-        return parsePrice(totalPrice);
+    public String getActualCountry() {
+        return actualCountry.getText();
     }
 
-    public double getExpectedPriceWithDiscount() {
-        double oldItemPrice = parsePrice(oldPrice);
-        double discount = Double.parseDouble(percent.getText().substring(1, -1));
-        return oldItemPrice * discount / 100;
+    public String getActualMobilePhone() {
+        return actualMobilePhone.getText();
     }
 
-    public void moveToSignIn() {
-        proceedToCheckOut.click();
+    public WebElement getProductItemsQty() {
+        return actualProductItemsQty;
     }
+
+    public BigDecimal getTotalPriceOfItems() {
+        BigDecimal priceOfFirstItem = parsePrice(actualPriceOfFirstItem);
+        BigDecimal priceOfSecondItem = parsePrice(actualPriceOfSecondItem);
+        return priceOfFirstItem.add(priceOfSecondItem);
+    }
+
+    public BigDecimal parsePrice(WebElement item) {
+        return new BigDecimal(item.getText().substring(1));
+    }
+
+    public BigDecimal getTotsProductsPrice() {
+        return parsePrice(totalProducts);
+    }
+
+    public BigDecimal getExpectedPriceWithDiscount() {
+        BigDecimal oldItemPrice = parsePrice(oldPrice);
+        BigDecimal discount = new BigDecimal(percent.getText().substring(1, -1));
+        return oldItemPrice.multiply(discount).divide(new BigDecimal(100));
+    }
+
 }
